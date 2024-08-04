a7737b7118acb236079c45717e05c4ed79fca6d8
provash10 <provash20cb@gmail.com>
2024-08-04 22:25:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// Check inspect console and #reload 
+
+// =========
+// Number
+
+// var num= 20;
+
+// console.log(typeof(num));
+
+// =============
+// toString
+// var num= "20";
+
+// num = toString(num)
+// console.log(typeof(num));
+
+// =======
+// parseInt
+
+// var num= "20";
+
+//  num = parseInt(num);
+// console.log(typeof(num));
+
+// ======
+// parsefloat
+// var num= "20.45";
+
+//  num = parsefloat(num);
+// console.log(typeof(num));
+
+// ===========================
+// #decimal ar por koto ghor deka jabe seta toFixed(2) 20.56
+var number = 2.5674;
+
+console.log(number.toFixed(2));
